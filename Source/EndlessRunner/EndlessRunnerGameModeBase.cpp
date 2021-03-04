c3353983947a303224c2418cddd10d3da0c8c897
Shankar Ghimire <ssghimire@gmail.com>
2021-03-03 21:06:31 -0500

    Sets up the Initial Floor Tiles at run time

--- Source/EndlessRunner/EndlessRunnerGameModeBase.cpp
+++ Source/EndlessRunner/EndlessRunnerGameModeBase.cpp
@@ -3,3 +3,31 @@
 
 #include "EndlessRunnerGameModeBase.h"
 
+
+void AEndlessRunnerGameModeBase::BeginPlay()
+{
+	CreateInitialTiles();
+}
+
+void AEndlessRunnerGameModeBase::CreateInitialTiles()
+{
+
+	for (int32 i = 0; i < NumInitialFloorTiles; i++)
+	{
+		AddFloorTile();
+	}
+}
+
+void AEndlessRunnerGameModeBase::AddFloorTile()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		AFloorTile* Tile = World->SpawnActor<AFloorTile>(FloorTileClass, NextSpawnPoint);
+		if (Tile)
+		{
+			NextSpawnPoint = Tile->GetAttachTransform();
+		}
+		
+	}
+}
--- Source/EndlessRunner/EndlessRunnerGameModeBase.h
+++ Source/EndlessRunner/EndlessRunnerGameModeBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FloorTile.h"
 #include "GameFramework/GameModeBase.h"
 #include "EndlessRunnerGameModeBase.generated.h"
 
@@ -14,4 +15,31 @@ class ENDLESSRUNNER_API AEndlessRunnerGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = "Config")
+		TSubclassOf<AFloorTile> FloorTileClass;
+
+	UPROPERTY(EditAnywhere, Category = "Config")
+		int32 NumInitialFloorTiles = 10;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Runtime")
+		FTransform NextSpawnPoint;
+
+	//UPROPERTY(VisibleInstanceOnly, Category = "Runtime")
+	//	TArray<float>LaneSwitchValues;
+
+
+	UFUNCTION(BlueprintCallable)
+		void CreateInitialTiles();
+
+	UFUNCTION(BlueprintCallable)
+		void AddFloorTile();
+
+
+protected:
+	virtual void BeginPlay() override;
+
+
 };
